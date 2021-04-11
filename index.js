fbd4471e675d3fbc70fae8c4eedbb64d6e6fe506
ahmed <ahm.gmal@gmail.com>
2021-04-11 14:24:36 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const input = document.getElementById("input");
+
 function addingEventListener() {
+    alert('I was clicked!');
+    input.addEventListener('click', clickAlert);
 }
+
+input.addEventListener('click', clickAlert);
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
